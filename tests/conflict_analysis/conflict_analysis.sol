b171f7647de669bdfdb50e0fa3894d806c7a13c6
kongqp <kongqp@mail2.sysu.edu.cn>
2022-02-21 10:42:34 +0800

    add the verification of correctness of conflict analysis

---
+++ tests/conflict_analysis/conflict_analysis.sol
@@ -0,0 +1,130 @@
+pragma solidity ^0.8.0;
+
+struct S {
+    uint256 a;
+    uint256 b;
+}
+
+contract BeCalledContract{
+    function getTime() public returns (uint) {
+        return block.timestamp;
+    }
+}
+
+contract ConflictTest{
+    uint256 public fixedSizeVar;
+    uint256[] public dynaArray;
+    mapping (uint256 => uint256) public simpleMap;
+    mapping (address => uint256) public simpleMapForTestEnv;
+    mapping (uint256 => S) public complexMap;
+    string public str;
+    S public simpleStruct;
+
+    constructor() {
+        fixedSizeVar = 1;
+        dynaArray.push(1);
+        simpleMap[1] = 1;
+        complexMap[2] = S(1,2);
+        str = "abc";
+        simpleStruct = S(1,2);  
+    }
+
+    // Fixed-sized（Booleans/Integers/Fixed Point Numbers/Address/Fixed-size byte arrays/...）
+    function fixedSizeVarRead() public returns (uint256){
+        return fixedSizeVar; //4, ConsConflict
+    }
+    function fixedSizeVarWrite() public {
+        fixedSizeVar = 2; //4, ConsConflict
+    }
+    
+    // Dynamically-sized (Dynamic Array/Mapping)
+    function dynaArrayReadWithConsKey() public returns (uint256){
+        return dynaArray[0]; //4, ConsConflict
+    }
+    function dynaArrayWriteWithConsKey() public {
+        dynaArray[0] = 3; //4, ConsConflict
+    }
+    function dynaArrayReadWithFuncArgKey(uint256 i) public returns (uint256){
+        return dynaArray[i]; //3, FunArgConflict
+    }
+    function dynaArrayWriteWithFuncArgKey(uint256 i) public {
+        dynaArray[i] = 1; //3, FunArgConflict
+    }
+    function simpleMapReadWithConsKey() public returns (uint256){
+        return simpleMap[1]; //4, ConsConflict
+    }
+    function simpleMapWriteWithConsKey() public {
+        simpleMap[2] = 3; //4, ConsConflict
+    }
+    function simpleMapReadWithEnvKey(uint x) public returns (uint256) {
+        if(x == 1){
+            return simpleMapForTestEnv[msg.sender]; //2, EnvConflict(CALLER)
+        }else{
+            if(x == 2){
+                return simpleMapForTestEnv[tx.origin]; //2, EnvConflict(ORIGIN)
+            }else{
+                if(x == 3){
+                    return simpleMapForTestEnv[address(this)]; //2, EnvConflict(ADDRESS)
+                }else{
+                    if(x == 4){
+                        return simpleMap[block.number]; //2, EnvConflict(NUMBER)
+                    }else{
+                        return simpleMap[block.timestamp]; //2, EnvConflict(TIMESTAMP)
+                    }
+                }
+            }
+        }
+    }
+    function simpleMapWriteWithEnvKey() public {
+        simpleMapForTestEnv[msg.sender] = 1; //2, EnvConflict(CALLER)
+        simpleMapForTestEnv[tx.origin] = 2; //2, EnvConflict(ORIGIN)
+        simpleMapForTestEnv[address(this)] = 3; //2, EnvConflict(ADDRESS)
+        simpleMap[block.number] = 4; //2, EnvConflict(NUMBER)
+        simpleMap[block.timestamp] = 5; //2, EnvConflict(TIMESTAMP)
+    }
+    function simpleMapReadWithFuncArgKey(uint256 i) public returns (uint256){
+        return simpleMap[i]; //3, FunArgConflict
+    }
+    function simpleMapWriteWithFuncArgKey(uint256 i) public {
+        simpleMap[i] = 1; //3, FunArgConflict
+    }
+    function simpleMapWriteWithPartStructFuncArgKey(S calldata s) public {
+        simpleMap[s.a] = 1; //3, FunArgConflict
+    }
+    function simpleMapIndirectWriteWithEnvKey() public {
+        uint a = block.number;
+        simpleMap[a] = 1; //2, EnvConflict(NUMBER)
+    }
+    function simpleMapWriteWithMixKey(uint x) public {
+        simpleMap[block.number + x] = 1; //0, MixConflict
+    }
+    function complexMapReadWithConsKey() public returns (uint256){
+        return complexMap[1].a; //4, ConsConflict
+    }
+    function complexMapWriteWithConsKey() public {
+        complexMap[3] = S(1,2); //4, ConsConflict
+    }
+
+    // Special (Struct/String)
+    function StrRead() public returns (string memory){
+        return str; //4, ConsConflict
+    }
+    function StrWrite() public {
+        str = "efg"; //4, ConsConflict
+    }
+    function simpleStructRead() public returns (uint256){
+        return simpleStruct.a; //4, ConsConflict
+    }
+    function simpleStructWrite() public {
+        simpleStruct.a = 2; //4, ConsConflict
+    }
+
+    // No storage access
+    function noStorageAccessAndContractCalling(uint x) public returns (uint) {
+        return x; //5, NoConflict
+    }
+    function noStorageAccessHasContractCalling(address addr) public returns (uint) {
+        return BeCalledContract(addr).getTime(); //0, NoStorageAccessHasContractCalling 
+    }
+
+}
---
+++ tests/conflict_analysis/verify_conflict.py
@@ -0,0 +1,79 @@
+import sys
+
+# Ground true of test cases
+ground_true_dict = {
+    "Conflict_ConsConflict.csv":[
+        ["0xd9fb343f", "0x679795a0195a1b76cdebb7c51d74e058aee92919b8c3389af86ef24535e8a28c"],
+        ["0xe2a57b9a", "0x6"],
+        ["0xd3619794", "0x0"],
+        ["0xac0f72ac", "0x1"],
+        ["0xb4a0b5e6", "0xe90b7bceb6e7df5418fb78d8ee546e97c83a08bbccc01a0644d599ccd2a7c2e0"],
+        ["0x845aba0c", "0x6"],
+        ["0x92bddf08", "0x1"],
+        ["0x9ba2a7c2", "0x1"],
+        ["0x9e935dc6", "0x1"],
+        ["0x6a446b88", "0xabd6e7cb50984ff9c2f3e18a2660c3353dadf4e3291deeb275dae2cd1e44fe05"],
+        ["0x7ea33fd1", "0x5"],
+        ["0x398fbc22", "0x0"],
+        ["0x4db0cdb2", "0x5"],
+        ["0x11eaad06", "0x2e174c10e159ea99b867ce3205125c24a42d128804e4070ed6fcc8cc98166aa0"]
+    ],
+    "Conflict_EnvConflict.csv":[
+        ["0xa8f3ceeb", "NUMBER", "MAP++[0x2]++0"],
+        ["0xe9f93afd", "CALLER", "MAP++[0x3]++0"],
+        ["0xe9f93afd", "ORIGIN", "MAP++[0x3]++0"],
+        ["0xe9f93afd", "ADDRESS", "MAP++[0x3]++0"],
+        ["0xe9f93afd", "NUMBER", "MAP++[0x2]++0"],
+        ["0xe9f93afd", "TIMESTAMP", "MAP++[0x2]++0"],
+        ["0xe6c96f77", "CALLER", "MAP++[0x3]++0"],
+        ["0xe6c96f77", "ORIGIN", "MAP++[0x3]++0"],
+        ["0xe6c96f77", "ADDRESS", "MAP++[0x3]++0"],
+        ["0xe6c96f77", "NUMBER", "MAP++[0x2]++0"],
+        ["0xe6c96f77", "TIMESTAMP", "MAP++[0x2]++0"]
+    ],
+    "Conflict_FunArgConflict.csv":[
+        ["0xeba52dda", "0", "MAP++[0x2]++0"],
+        ["0xc158e6ef", "0", "MAP++[0x2]++0"],
+        ["0xac0f72ac", "0", "ARRAY++[0x1]++0"],
+        ["0x9e935dc6", "0", "ARRAY++[0x1]++0"],
+        ["0x1bfdff", "0", "MAP++[0x2]++0"]
+    ],
+    "Conflict_MixConflict.csv":[
+        ["0x5ed8a780", "MAP++[0x2]++0"],
+        ["0x92bddf08", "ARRAY++[0x1]++0"],
+        ["0x9ba2a7c2", "ARRAY++[0x1]++0"],
+        ["0x7ea33fd1", "ARRAY++[0x5]++0"],
+    ],
+    "Conflict_NoConflict.csv":[
+        ["0x16c5ae5e"]
+    ],
+    "Conflict_NoStorageAccessHasContractCalling.csv":[
+        ["0xe7ba68d0"]
+    ]
+}
+num_success_analysis = 0
+for file_name in ground_true_dict.keys():
+    num_match_ground_true = 0
+    with open("./.temp/conflict_analysis-opt/out/" + file_name, "r") as f:
+        lines = f.readlines()
+    for i in range(len(ground_true_dict[file_name])):
+        stop_flag = True
+        gt = "\t".join(ground_true_dict[file_name][i])
+        for line in lines:
+            if gt in line:
+                stop_flag = False
+                break
+        if stop_flag:
+            print("Error: " + file_name + " not as expected.")
+            break
+        else:
+            num_match_ground_true += 1
+    if num_match_ground_true == len(ground_true_dict[file_name]):
+        num_success_analysis +=1
+    else:
+        break
+
+if num_success_analysis == len(ground_true_dict):
+    sys.exit(0)
+else:
+    sys.exit(1)
